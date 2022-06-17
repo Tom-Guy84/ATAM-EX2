3691930ff39c35b16f3822681783e4d0919d5dd1
zivdamir <73081641+zivdamir@users.noreply.github.com>
2022-06-17 12:05:11 +0300

    Add files via upload

--- ili_handler.asm
+++ ili_handler.asm
@@ -3,13 +3,27 @@
 .text
 .align 4, 0x90
 my_ili_handler:
-    pushq %r12
-    pushq %r13
+    push %rax
+    push %rdi
+    push %rsi
+    push %rdx
+    push %rcx
+    push %r8
+    push %r9
+    push %r10
+    push %r11
+    push %rbx
+    push %rsp
+    push %rbp
+    push %r12
+    push %r13
+    push %r14
+    push %r15
     xorq %r12, %r12 #tp change the rip in the needed offset
     xorq %rdi, %rdi
     xorq %rcx, %rcx
-    movq 16(%rsp), %rax #rax is users rip
-    leaq 16(%rsp), %r13 #rip to change
+    movq 128(%rsp), %rax #rax is users rip
+    leaq 128(%rsp), %r13 #rip to change
     movb (%rax), %cl
     cmpb $0x0f, %cl
     je opcode_is_last_byte
@@ -26,13 +40,43 @@ call_what_to_do:
     cmp $0, %rdi
     jne return_from_handler
     
-    popq %r13
-    popq %r12
+    pop %r15
+    pop %r14
+    pop %r13
+    pop %r12
+    pop %rbp
+    pop %rsp
+    pop %rbx
+    pop %r11
+    pop %r10
+    pop %r9
+    pop %r8
+    pop %rcx
+    pop %rdx
+    pop %rsi
+    pop %rdi 
+    pop %rax
     jmp *old_ili_handler
     
 return_from_handler:   
     addq %r12, (%r13)
-    popq %r13
-    popq %r12
-      
+    
+    pop %r15
+    pop %r14
+    pop %r13
+    pop %r12
+    pop %rbp
+    pop %rsp
+    pop %rbx
+    pop %r11
+    pop %r10
+    pop %r9
+    pop %r8
+    pop %rcx
+    pop %rdx
+    pop %rsi
+    pop %rax 
+    pop %rax
+   
+
   iretq
